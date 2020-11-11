aef20e5ad6df37caf55355a8f62be2f27b24cb61
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-11-11 17:11:33 +0000

    notes on initial setup

---
+++ setup_notes.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+id=`id -u`
+
+if [ $id = "0" ]
+then
+
+    echo "Root setup"
+    apt-get install python3 python3-pip gcc unzip wget zip gcc-avr binutils-avr avr-libc dfu-programmer dfu-util gcc-arm-none-eabi binutils-arm-none-eabi avrdude libusb-dev
+
+    cat << EOF >> /etc/udev/rules.d/50-wally.rules
+# Teensy rules for the Ergodox EZ
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+EOF
+
+else
+
+    python3 -m pip install --user qmk
+
+    export PATH=$PATH:~/.local/bin
+    qmk setup
+    
+    wget https://github.com/zsa/wally-cli/releases/download/2.0.0-linux/wally-cli -O ~/.local/bin/wally-cli
+    chmod +x ~/.local/bin/wally-cli
+fi
+
