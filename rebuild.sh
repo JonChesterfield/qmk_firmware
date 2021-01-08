bb76dfca3d0c75eae33d77d7cc4d008091eac59c
Jon Chesterfield <jonathanchesterfield@gmail.com>
2021-01-08 22:57:20 +0000

    quote zip file

--- rebuild.sh
+++ rebuild.sh
@@ -17,7 +17,7 @@ SRCZIP=`ls -t  ~/Downloads/ergodox_ez_shine_*.zip  | head -n 1`
 
 echo "Reading from src $SRCZIP"
 
-unzip $SRCZIP -d $TMP/
+unzip "$SRCZIP" -d $TMP/
 
 for s in keymap.c config.h; do
     cp $TMP/ergodox_ez_shine_jon_source/$s keyboards/ergodox_ez/keymaps/jon/
